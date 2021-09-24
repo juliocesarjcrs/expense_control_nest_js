31886087610983f842e25da15f7793643e4f38ba
Julio Rico <Julio.Rico@bitsamericas.com>
2021-09-24 14:35:11 -0500

    capure error

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,17 +7,21 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserCode(user: User, code: number) {
-    const resp = await this.mailerService.sendMail({
-      to: user.email,
-      // from: '"Support Team" <support@example.com>', // override default from
-      subject: '¡Bienvenido App Control gastos! Código de recuperación',
-      template: './sendCode', // `.hbs` extension is appended automatically
-      context: {
-        // ✏️ filling curly brackets with content
-        name: user.name,
-        code,
-      },
-    });
-    return resp;
+    try {
+      const resp = await this.mailerService.sendMail({
+        to: user.email,
+        // from: '"Support Team" <support@example.com>', // override default from
+        subject: '¡Bienvenido App Control gastos! Código de recuperación',
+        template: './sendCode', // `.hbs` extension is appended automatically
+        context: {
+          // ✏️ filling curly brackets with content
+          name: user.name,
+          code,
+        },
+      });
+      return resp;
+    } catch (error) {
+      return error;
+    }
   }
 }
