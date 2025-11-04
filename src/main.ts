4af6fc33b16b3f5bf57c7b12202201896b3a7607
juliocesarjcrs <jcrico27@misena.edu.co>
2025-11-04 07:47:09 -0500

    fix(core): remove global crypto assignment to avoid TypeError in Node 22

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import * as crypto from 'crypto';
-(global as any).crypto = crypto;
-
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
